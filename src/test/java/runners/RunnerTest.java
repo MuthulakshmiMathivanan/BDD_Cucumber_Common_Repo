af681b7d5b07b9c4791d4582c8513eebd7e987a3
Princy Prabhakaran S <princy.prabhakarans@expleogroup.com>
2025-04-15 13:09:35 +0530

    on my branch commit

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -17,8 +17,8 @@ import io.cucumber.junit.Cucumber;
               "html:target/cucumber-reports.html",
               "json:target/cucumber-reports.json",
               "junit:target/cucumber-reports.xml"},
-                dryRun=false
-                //tags = "@PrincyCart"
+                dryRun=false,
+                tags = "@Princy"
         )
 
 public class RunnerTest {
