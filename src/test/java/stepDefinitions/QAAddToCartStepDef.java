66cbf6422fb7d9f53bf5709dbc19bd0a7fd0ec39
Muthulakshmi Mathivanan <muthulakshmimathivanan@gmail.com>
2025-04-15 16:51:03 +0530

    Princy scenarios added

---
+++ src/test/java/stepDefinitions/QAAddToCartStepDef.java
@@ -0,0 +1,40 @@
+package stepDefinitions;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.*;
+
+public class QAAddToCartStepDef {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Given("I am on the Swag Labs inventory page")
+    public void i_am_on_the_swag_labs_inventory_page() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        // Ensure you're on the inventory page
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(currentUrl.contains("inventory"));
+    }
+
+    @When("I click the \"Add to cart\" button for \"Sauce Labs Backpack\"")
+    public void i_click_add_to_cart_for_sauce_labs_backpack() {
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+    }
+
+    @Then("the cart badge should display \"1\"")
+    public void the_cart_badge_should_display_1() {
+        WebElement cartBadge = driver.findElement(By.className("shopping_cart_badge"));
+        assertEquals("1", cartBadge.getText());
+    }
+
+    
+}
