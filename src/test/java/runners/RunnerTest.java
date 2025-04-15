5042b972d967dc4538a55876167b47048548507a
Princy Prabhakaran S <princy.prabhakarans@expleogroup.com>
2025-04-15 07:18:34 +0530

    added scenarios

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -17,8 +17,8 @@ import io.cucumber.junit.Cucumber;
               "html:target/cucumber-reports.html",
               "json:target/cucumber-reports.json",
               "junit:target/cucumber-reports.xml"},
-                dryRun=false,
-                tags = "@Princy"
+                dryRun=false
+                //tags = "@PrincyCart"
         )
 
 public class RunnerTest {
