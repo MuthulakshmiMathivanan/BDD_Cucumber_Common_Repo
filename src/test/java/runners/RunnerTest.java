0e332eacc246e222719d0da72a9e6a041ee60bf7
rajasekar-inbasekaran-expleo <rajasekar.inbasekaran@expleogroup.com>
2025-04-16 20:17:37 +0530

    changes for error in step def

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -10,14 +10,14 @@ import io.cucumber.junit.Cucumber;
  
 @RunWith(Cucumber.class)
 @CucumberOptions(
- features = "src/test/resources",
+ features = "src/test/resources/Rajasekar_15APR2025_SauceCheckout.feature",
         
         glue= {"stepDefinitions"},
         plugin = {"pretty", 
               "html:target/cucumber-reports.html",
               "json:target/cucumber-reports.json",
               "junit:target/cucumber-reports.xml"},
-                   dryRun=false
+                   dryRun=false     
         )
 public class RunnerTest {
 }
