fc0f217fde1e3f639302349d2e26d01f4c3d9473
Yugendranjayakumar <yugendranjayakumar2001@gmail.com>
2025-04-22 12:05:39 +0530

    change

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,17 +1,28 @@
+ 
 package runners;
-
+ 
+ 
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+ 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",  // Use relative path
-    glue = {"stepDefinitions"}, // Ensure correct package
-    plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"},
-    monochrome = true,
-    dryRun = false
-)
+ features = "src/test/resources/features",
+        
+        glue= {"stepDefinitions"},
+        plugin = {"pretty", 
+              "html:target/cucumber-reports.html",
+              "json:target/cucumber-reports.json",
+              "junit:target/cucumber-reports.xml"},
 
+
+                   dryRun=false
+                   
+
+ )
 public class TestRunner {
+	
 }
+ 
